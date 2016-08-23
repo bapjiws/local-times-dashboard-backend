c47474b7ef2839b6c57cfdd5f6da7532dd82ed2b
alexboklin <alex.boklin@gmail.com>
2016-08-23 22:13:45 +0300

    Add City model

---
+++ revel_app/app/models/city.go
@@ -0,0 +1,16 @@
+package models
+
+// TODO: remove when done
+/*
+Country,City,AccentCity,Region,Population,Latitude,Longitude
+us,new york,New York,NY,8107916,40.7141667,-74.0063889
+ru,moscow,Moscow,48,10381288,55.752222,37.615556
+*/
+
+// This struct partially maps to the ones that we get from our city database: https://www.maxmind.com/en/free-world-cities-database
+type City struct {
+	CountryCode string  `json:"country"` // aka "Country"
+	Name        string  `json:"name"`    // aka "AccentCity"
+	Latitude    float64 `json:"latitude"`
+	Longitude   float64 `json:"longitude"`
+}
