83a8c32f4ef4b0a0af61c194ab31deca3509c582
alexboklin <alex.boklin@gmail.com>
2016-08-23 23:52:23 +0300

    Add AddDocument function + TODOs

--- datastore/elasticsearch/city.go
+++ datastore/elasticsearch/city.go
@@ -3,6 +3,7 @@ package elasticsearch
 import (
 	"fmt"
 
+	"../../revel_app/app/models"
 	"gopkg.in/olivere/elastic.v3"
 )
 
@@ -22,8 +23,11 @@ var mapping = `{
 }`
 
 //type CityIndex struct {
+//	Client *elastic.Client
 //	Mapping string
 //}
+
+// TODO: implement NewCityIndex function
 
 func CreateIndex() error { // TODO: make it a method a with pointer receiver?
 	client, err := elastic.NewClient()
@@ -31,7 +35,7 @@ func CreateIndex() error { // TODO: make it a method a with pointer receiver?
 		return fmt.Errorf("Couldn't create a client: %s.\n", err.Error())
 	}
 
-	createIndex, err := client.CreateIndex("city").BodyString(mapping).Do()
+	createIndex, err := client.CreateIndex("city").BodyString(mapping).Do() // TODO: abstract this away via index interface?
 	if err != nil {
 		// TODO: Handle error
 		panic(err)
@@ -39,6 +43,28 @@ func CreateIndex() error { // TODO: make it a method a with pointer receiver?
 	if !createIndex.Acknowledged {
 		// TODO: Not acknowledged
 	}
+
+	return nil
+}
+
+func AddDocument(city models.City) error {
+	// TODO: creating a client for the second time, make this a method?
+	client, err := elastic.NewClient()
+	if err != nil {
+		return fmt.Errorf("Couldn't create a client: %s.\n", err.Error())
+	}
+
+	put, err := client.Index().
+		Index("timezones").
+		Type("city").
+		Id("1"). // TODO: work this out
+		BodyJson(city).
+		Do()
+	if err != nil {
+		// TODO: Handle error
+		panic(err)
+	}
+	fmt.Printf("Indexed tweet %s to index %s, type %s\n", put.Id, put.Index, put.Type)
 
 	return nil
 }
