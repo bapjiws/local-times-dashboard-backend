e9f33ba7bd4eeb60faec181f2d62aa9ce74faab9
bapjiws <alex.boklin@gmail.com>
2016-09-08 21:33:48 +0300

    Add test SearchCity method to the App controller + route

--- revel_app/app/controllers/app.go
+++ revel_app/app/controllers/app.go
@@ -9,3 +9,9 @@ type App struct {
 func (c App) Index() revel.Result {
 	return c.Render()
 }
+
+func (c App) SearchCity(name string) revel.Result{
+	response := make(map[string]interface{})
+	response["the_city_you_searched_for"] = name
+	return c.RenderJson(response)
+}
