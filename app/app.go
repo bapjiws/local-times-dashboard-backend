d81733bfeb754804ca419ed3fefc3c2cf71d48ee
bapjiws <alex.boklin@gmail.com>
2016-09-22 21:58:53 +0300

    Create cityRouter; clean the code

--- app/app.go
+++ app/app.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	//"net/http"
 	"github.com/bapjiws/timezones_mc/app/handlers"
 	"github.com/bapjiws/timezones_mc/app/middleware"
 	"github.com/bapjiws/timezones_mc/datastore/elasticsearch"
 	"github.com/bapjiws/timezones_mc/datastore/elasticsearch/configs"
 	"github.com/gin-gonic/gin"
-	//"github.com/itsjamie/gin-cors"
-	//"time"
 )
 
 const (
-	APIBASE = "api" // TODO: rename into CITY_API_BASE
-	PORT    = ":8888"
+	API_BASE = "api"
+	PORT     = ":8888"
 )
 
 var ES *elasticsearch.ElasticStore
@@ -29,28 +26,10 @@ func main() {
 	router := gin.Default()
 
 	router.Use(middleware.SetContext(ES))
-	//, cors.Middleware(cors.Config{
-	//	Origins:        "*",
-	//	Methods:        "GET, PUT, POST, DELETE",
-	//	RequestHeaders: "Origin, Authorization, Content-Type, access-control-allow-origin",
-	//	ExposedHeaders: "",
-	//	MaxAge: 50 * time.Second,
-	//	Credentials: true,
-	//	ValidateHeaders: false,
-	//})
 
-	router.GET("/city", handlers.SuggestCities)
-	router.GET("/city/:id", handlers.FindCityById)
-
-	routerGroup := router.Group(APIBASE)
-	//routerGroup.OPTIONS("/city", preflight)
-	routerGroup.GET("/city", handlers.SuggestCities)
+	cityRouter := router.Group(API_BASE)
+	cityRouter.GET("/city", handlers.SuggestCities)
+	cityRouter.GET("/city/:id", handlers.FindCityById)
 
 	router.Run(PORT)
 }
-
-//func preflight(c *gin.Context) {
-//	c.Header("Access-Control-Allow-Origin", "*")
-//	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin") // , access-control-allow-headers
-//	c.JSON(http.StatusOK, struct{}{})
-//}
